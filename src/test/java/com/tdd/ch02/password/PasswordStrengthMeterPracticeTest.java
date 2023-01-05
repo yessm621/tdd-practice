ad2d6a26bf1f9f386fab7fe6e2d54bf3b31f53b9
yessm621 <yessm621@gmail.com>
2023-01-05 18:00:21 +0900

    패스워드 암호 강도 연습하기

---
+++ src/test/java/com/tdd/ch02/password/PasswordStrengthMeterPracticeTest.java
@@ -0,0 +1,22 @@
+package com.tdd.ch02.password;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 암호 검사기
+ * 규칙
+ * - 길이가 8글자 이상
+ * - 0 부터 9 사이의 숫자를 포함
+ * - 대문자 포함
+ * 3 규칙을 모두 만족하면 암호는 강함
+ * 2 규칙을 만족하면 암호는 보통
+ * 1 규칙 이하를 만족하면 암호는 약함
+ */
+public class PasswordStrengthMeterPracticeTest {
+
+    @Test
+    void name() {
+
+    }
+}
